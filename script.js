3300108b50653974facbdb6f4708dda1bdcfaa4b
UyashaISHINI <uyashaishini@gmail.com>
2025-06-05 20:32:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Navbar CSS property is scrolled
+window.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 100) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// profile section redirection
+const profileIcon = document.querySelector(".profile-icon");
+profileIcon.addEventListener("click", () => {
+  window.location.href = "profile-details.html";
+});
+
+// Movie Card
+const movieCard = document.querySelectorAll(".content-card");
+movieCard.forEach((card) => {
+  card.addEventListener("mouseEnter", () => {
+    card.style.transform = "scale(1.05)";
+  });
+  card.addEventListener("mouseLeave", () => {
+    card.style.transform = "scale(1)";
+  });
+});
